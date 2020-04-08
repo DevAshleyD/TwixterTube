0a5bcfec125e80506ce95ef284c4df975575063c
ashap94 <aaronshapiro94@gmail.com>
2020-04-07 22:18:50 -0700

    Added functionlity for items not in a row that is maxed out horizontally in elements to initially render with that offsetwidth and offsetheight of the first element in the list
    the first element retains dynamic functionality at all times and dictates all other elements behavior with intial renders and resizing of browser

--- frontend/components/videos/video_index.jsx
+++ frontend/components/videos/video_index.jsx
@@ -8,20 +8,106 @@ import ModalSideBarContainer from "../sidebar/modal_sidebar_container";
 class VideosIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loaded: false,
+      // windowListener: false,
+    };
+    this.windowListener = false;
+    // this.firstVidRef = React.createRef();
+    this.videoListRef = React.createRef();
+    this.handleLoad = this.handleLoad.bind(this);
+    this.load = false;
+    this.usedFunctionWithLoad = false;
+    this.windowResiszeListener = this.windowResiszeListener.bind(this);
   }
 
   componentDidMount() {
     window.scrollTo(0, 0);
 
     this.props.fetchVideos();
-    console.log(
-      "WHAT DOES URL LOOK LIKE HERE IN INDEX COMPONENT:   ",
-      this.props.match
-    );
+
+    this.setState({
+      loaded: true,
+    });
+  }
+
+  componentDidUpdate() {
+    if (this.load === true && this.usedFunctionWithLoad === false) {
+      this.handleLoad();
+      this.usedFunctionWithLoad = true;
+    }
+  }
+
+  handleLoad() {
+    let width = this.videoListRef.current.children[0].offsetWidth;
+    let height = this.videoListRef.current.children[0].offsetHeight;
+
+    let children = Object.values(this.videoListRef.current.children);
+
+    for (let i = 1; i < children.length; i++) {
+      let child = children[i];
+      if (child.offsetWidth !== width || child.offsetHeight !== height) {
+        child.style.maxWidth = width + "px";
+        child.style.maxHeight = height + "px";
+      }
+    }
+  }
+
+  windowResiszeListener() {
+    window.addEventListener("resize", () => {
+      // console.log("HERE IS THE FIRST VIDEO:    ", this.firstVidRef);
+      // console.log("HERE IS THE VIDEO LIST REF:    ", this.videoListRef);
+      // console.log(
+      //   "HERE IS CHILDREN CHILD:    ",
+      //   this.videoListRef.current.children[8]
+      // );
+      // console.log(
+      //   "WHAT IS THE TYPE OF THE CHILDREN:    ",
+      //   typeof Object.values(this.videoListRef.current.children)
+      // );
+
+      this.handleLoad();
+
+      // this.videoListRef.current.children[8].style.maxWidth = width + "px";
+      // this.videoListRef.current.children[8].style.maxHeight = height + "px";
+
+      // let style = {
+      //   width: width + "px",
+      //   height: height + "px",
+      // };
+    });
+
+    // this.videoListRef.current.addEventListener("load", () => {
+    //   let width = this.videoListRef.current.children[0].offsetWidth;
+    //   let height = this.videoListRef.current.children[0].offsetHeight;
+
+    //   let children = Object.values(this.videoListRef.current.children);
+
+    //   for (let i = 1; i < children.length; i++) {
+    //     let child = children[i];
+    //     if (child.offsetWidth !== width || child.offsetHeight !== height) {
+    //       child.style.maxWidth = width + "px";
+    //       child.style.maxHeight = height + "px";
+    //     }
+    //   }
+    // });
+    if (this.windowListener === false) {
+      this.windowListener = true;
+    }
   }
 
   render() {
-    let videos = this.props.videos.map(video => {
+    let videos = this.props.videos.map((video, idx) => {
+      // if (idx === 0 && !!video) {
+      //   return (
+      //     <VideoIndexItem
+      //       video={video}
+      //       key={video.id}
+      //       uploaders={this.props.uploaders}
+      //       // ref={this.firstVidRef}
+      //     />
+      //   );
+      // }
       return (
         <VideoIndexItem
           video={video}
@@ -31,17 +117,12 @@ class VideosIndex extends React.Component {
       );
     });
 
+    if (videos.length !== 0 && this.windowListener === false) {
+      this.windowResiszeListener();
+      this.load = true;
+    }
+
     videos.sort(() => Math.random() - 0.5);
-
-    // let videos = this.props.videos.map( video => {
-
-    //     return (
-    //         <VideoIndexItem
-    //         video={video}
-    //         key={video.id}
-    //         />
-    //     )
-    // })
 
     return (
       <div>
@@ -57,7 +138,12 @@ class VideosIndex extends React.Component {
                   <h1>Recommended</h1>
                 </div>
                 <div className="video-index-container">
-                  <ul className="video-index-container-items">{videos}</ul>
+                  <ul
+                    className="video-index-container-items"
+                    ref={this.videoListRef}
+                  >
+                    {videos}
+                  </ul>
                 </div>
               </div>
             </div>
